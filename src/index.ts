e9fb3285196be2f72c6832dc69c3293538a4b6cc
Joe Martinez <notjoemartinez@tutanota.com>
2024-08-13 10:13:56 -0500

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,56 @@
-import {
-	Env
-} from './types';
+import { Env } from './types';
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    // Handle CORS preflight requests
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: getCorsHeaders(),
+      });
+    }
 
-		if (request.method != 'GET') {
-			return new Response('Method Not Allowed', {
-				status: 405,
-				headers: {
-				  Allow: 'GET',
-				},
-			});
-		}
+    if (request.method !== 'GET') {
+      return new Response('Method Not Allowed', {
+        status: 405,
+        headers: getCorsHeaders()
+      });
+    }
 
-		const url = new URL(request.url);
-		const pathParts = url.pathname.split('/');
-		const parentPath = `${pathParts[1]}/${pathParts[2]}`;
+    const url = new URL(request.url);
+    const pathParts = url.pathname.split('/');
+    const parentPath = `${pathParts[1]}/${pathParts[2]}`;
 
-		if (parentPath != 'yt-fts/test_dbs'){
-			return new Response('Object Not Found', { 
-				status: 404 
-			});
-		}
+    if (parentPath !== 'yt-fts/test_dbs') {
+      return new Response('Object Not Found', { 
+        status: 404,
+        headers: getCorsHeaders()
+      });
+    }
 
+    const key = `${pathParts[2]}/${pathParts[3]}`; 
+    const object = await env.YT_FTS.get(key);
 
-		const key = `${pathParts[2]}/${pathParts[3]}`; 
-		const object = await env.YT_FTS.get(key);
+    if (object === null) {
+      return new Response('Object Not Found', { 
+        status: 404,
+        headers: getCorsHeaders()
+      });
+    }
 
-		if (object === null) {
-			return new Response('Object Not Found', { status: 404 });
-		}
+    const headers = new Headers(getCorsHeaders());
+    object.writeHttpMetadata(headers);
+    headers.set('etag', object.httpEtag);
 
-		const headers = new Headers();
-		object.writeHttpMetadata(headers);
-		headers.set('etag', object.httpEtag);
-
-		return new Response(object.body, {
-			headers,
-		});
-		
-	},
+    return new Response(object.body, {
+      headers,
+    });
+  },
 } satisfies ExportedHandler<Env>;
+
+function getCorsHeaders(): HeadersInit {
+  return {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+}
